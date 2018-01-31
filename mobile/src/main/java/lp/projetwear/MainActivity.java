81b09842a3c0170fff037a6ba443ec3ac6449c3a
antoine.py@univ-fcomte.fr <guildantoine@hotmail.fr>
2018-01-31 15:56:42 +0100

    base and emailpassword ACTIVITY OHOHOHOHOHO

--- mobile/src/main/java/lp/projetwear/MainActivity.java
+++ mobile/src/main/java/lp/projetwear/MainActivity.java
@@ -1,13 +1,32 @@
 package lp.projetwear;
 
+import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+        private FirebaseAuth mAuth;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        public void onComplete(Task<AuthResult> task) {
+
+            }
+
+            // ...
+
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
     }
+
 }
